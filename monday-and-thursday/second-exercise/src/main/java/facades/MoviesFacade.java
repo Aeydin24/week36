72ce83a4bdfed49cdf4532d4a678624d441db8ec
Benjamin Bajrami <Aeydin24@gmail.com>
2019-09-05 20:35:18 +0200

    changed folder name

---
+++ monday-and-thursday/second-exercise/src/main/java/facades/MoviesFacade.java
@@ -0,0 +1,61 @@
+package facades;
+
+import entities.Movies;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class MoviesFacade {
+
+    private static MoviesFacade instance;
+    private static EntityManagerFactory emf;
+
+    //Private Constructor to ensure Singleton
+    private MoviesFacade() {
+    }
+
+    /**
+     *
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static MoviesFacade getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new MoviesFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public Movies findMovie(int id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Movies movie = em.find(Movies.class, id);
+            return movie;
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Movies> getAllMovies() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery<Movies> query
+                    = em.createQuery("Select movie from Movies movie", Movies.class);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ monday-and-thursday/second-exercise/target/generated-sources/annotations/entities/Movies_.java
@@ -0,0 +1,16 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-03T19:29:44")
+@StaticMetamodel(Movies.class)
+public class Movies_ { 
+
+    public static volatile SingularAttribute<Movies, String[]> actors;
+    public static volatile SingularAttribute<Movies, Integer> year;
+    public static volatile SingularAttribute<Movies, String> name;
+    public static volatile SingularAttribute<Movies, Long> id;
+
+}
