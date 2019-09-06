13cb9b82930794b9324c82861995c2af50cfbd1a
Benjamin Bajrami <Aeydin24@gmail.com>
2019-09-06 22:31:49 +0200

    Added colorchange and bubbling exercise.

---
+++ Thursday/Week36thursday/public_html/Javascript/colorchange.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*  a) When the page initially is loaded all divs should be 
+ *  given a colour of your choice
+ *  Hints: use document.getElementsByTagName() 
+ *  (will return an array of Dom Nodes) and 
+ *  element.style.backgroundColor 
+ */
+function divcolor() {
+    let divArray = Array.from(document.getElementsByTagName('div'));
+    divArray.forEach(element => {
+        element.style.backgroundColor = "blue";
+    });
+}
+
+/*
+ *  b) Add a button, and assign a click handler to the button. 
+ *  When the button is clicked each div should be given 
+ *  a unique colour.
+ *  Hints: use document.getElementById(..) 
+ *  to get the individual div’s
+ */
+function colourChange() {
+    document.getElementById('div 1').style.backgroundColor = "green";
+    document.getElementById('div 2').style.backgroundColor = "blue";
+    document.getElementById('div 3').style.backgroundColor = "red";
+}
